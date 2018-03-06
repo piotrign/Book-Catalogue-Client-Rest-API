b3e9a5fdd2174cd02ed6075725cb4e8292d0928d
piotrign <piotrmignasiak@gmail.com>
2018-03-06 22:01:51 +0100

    Warszat 4 po zajeciach - do dokonczenia

---
+++ zadanie.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $.ajax({
+      url: 'https://date.jsontest.com',
+      data: '{}',
+      type: 'GET',
+      dataType: "json",
+    success: function(json) {
+      console.log(json);
+    }
+    error: function(xhr, status, errorThrown){
+      console.log(status);
+    }
+    complete:funciton(xhr, status){
+      console.log("done");
+    }
+  });
+//zad 3
+//scrypt jqueyr
+//scrypt nasz
+// Porada na przyszlosc:
+// serwer serwuje jsony
+// i fron rysuje UI
+
+});
---
+++ zadanie3.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+$.ajax({
+      url: 'http://localhost:8282/books',
+      data: '{}',
+      type: 'GET',
+      dataType: "json",
+    success: function(json) {
+      console.log(json);
+
+        for (var i = 0; i < json.length; i++) {
+          var book = $('<li>');
+            book.addClass('list-group-item');
+
+
+          var title =$('<a>').text(json[i].title);
+            title.addClass('list-group-item list-group-item-action');
+
+
+            //console.log(json[i].id);
+
+            var id = $('<div>').text(json[i].id);
+            id.addClass('list-group-item');
+            id.addClass('idToggle')
+
+            $(document).on('click',".list-group-item", function(e){
+              e.preventDefault();
+              $(".idToggle").toggle(2000);
+            });
+              //   title.text("<div>" + id).toggle();
+              // });
+
+          var author =$('<div>').text(json[i].author);
+            author.addClass('list-group-item');
+          var publisher =$('<div>').text(json[i].publisher);
+            publisher.addClass('list-group-item');
+
+          book.append(title, id, author, publisher);
+          $('#bookList').append(book);
+        }
+    },
+    error: function(xhr, status, errorThrown){
+      console.log(status);
+    },
+    complete: function(xhr, status){
+      console.log("done");
+    }
+
+  });
+  });
